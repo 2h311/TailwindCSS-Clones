85552c4d4a3b3a6facfc5a87b40249f801a18528
2h311 <kcodepal@outlook.com>
2024-05-17 13:42:28 +0100

    ContactSection.jsx: just getting started on this HOC

---
+++ src/pages/ContactSection.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { ChevronDownIcon } from "../assets/icons";
+import { Switch } from "@headlessui/react";
+
+const classNames = (...classes) => {
+  return classNames.filter(Boolean).join(" ");
+};
+
+const ContactSection = () => {
+  return <div>ContactSection</div>;
+};
+
+export default ContactSection;
